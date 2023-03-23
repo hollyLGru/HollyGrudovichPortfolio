2b16e86d93642f45f3041558326bfa937885220b
HollyLgru <H.grudovich@zollege.com>
2023-03-23 13:31:22 -0500

    lala

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 // import Image from 'next/image'
-import Link from "next/link"
+import Link from "next/link";
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
 
@@ -26,6 +27,14 @@ const Navbar = () => {
                             <li className='ml-10 text-sm uppercase hover:border-b'>contact</li>
                         </Link>
                     </ul>
+                    <div className='md:hidden'>
+                        <AiOutlineMenu size={25}/>
+                    </div>
+                </div>
+            </div>
+            <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+                <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[white] p-10 ease-in duration-500'>
+
                 </div>
             </div>
         </div>
