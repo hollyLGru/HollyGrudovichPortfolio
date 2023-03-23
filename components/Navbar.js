05b05fcbd0cbfbb8da0ec48dc05bc1904394c81d
HollyLgru <H.grudovich@zollege.com>
2023-03-23 13:16:47 -0500

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 // import Image from 'next/image'
+import Link from "next/link"
 
 const Navbar = () => {
 
@@ -7,6 +8,25 @@ const Navbar = () => {
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 {/* <Image src='/../public/logo.png' alt='' width='125' height='50' margin-bottom='25%'/> */}
                 <h2 className='tracking-widest text-[#d17da7] drop-shadow-xl'>Holly Grudovich</h2>
+                <div>
+                    <ul className='hidden md:flex'>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>HOME</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>skills</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>projects</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>contact</li>
+                        </Link>
+                    </ul>
+                </div>
             </div>
         </div>
     )
